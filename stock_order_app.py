0e4879b9e63e8d2330a543dd70e6ec290481618f
brianbrainbrian <clisteengineering@gmail.com>
2025-04-10 20:21:55 +1000

    Update stock_order_app.py

--- stock_order_app.py
+++ stock_order_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 from io import BytesIO
 from openpyxl import Workbook
 
+st.title("📦 Technician stock assist")
+
 # Step 1: Request user's full name
 user_name = st.text_input("Please enter your full name to begin:")
 
